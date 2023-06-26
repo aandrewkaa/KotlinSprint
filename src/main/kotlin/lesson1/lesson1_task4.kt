661d68a6d1e73aa6373572288224ae3e64e37621
Aandrewkaa <sa9035837863@gmail.com>
2023-06-25 23:29:21 -0300

    KS-1-4 solution task4

---
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -0,0 +1,27 @@
+package lesson1
+
+/** Created by Aandrewkaa **/
+
+/* программа с иформацией о
+полете Юрия Гагарина в космос
+ */
+
+fun main() {
+    val length: Long = 40_868_600_000L
+    val age: UByte = 27U
+    val partOfDay: Float = 0.075F
+    val seconds: UShort = 6480U
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: UInt = 327000U
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
+
+
+
