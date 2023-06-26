40c1badcdc2e267bb149c18ef5d5b94e495337b6
Aandrewkaa <sa9035837863@gmail.com>
2023-06-25 23:14:09 -0300

    KS-1-3 solution task3

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,26 @@
+package lesson1
+
+import kotlin.math.min
+
+
+/** Created by Aandrewkaa **/
+
+/* программа с иформацией о дате и времени
+полета Юрия Гагарина в космос
+ */
+
+fun main() {
+    val year: UShort = 1961U
+    var hour: UShort = 9U
+    var minute: UShort = 7U
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10U
+    minute = 55U
+
+    println(hour.toString()+":"+minute.toString())
+
+}
