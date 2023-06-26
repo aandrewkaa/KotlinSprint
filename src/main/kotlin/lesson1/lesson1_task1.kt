963e4d57da34d373491ab2abec9b481c6f2b5876
Aandrewkaa <sa9035837863@gmail.com>
2023-06-25 22:37:26 -0300

    KS-1-1 change type for orders

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -3,6 +3,7 @@ package lesson1
 /** Created by Aandrewkaa **/
 
 fun main() {
-
     println("Это решение задачи из 1 из урока 1 ")
+    var number_of_orders: UShort = 75U //var т.к. количество заказов меняется
+    val gratitude_for_purchase: String = "Thank you for purchase!" //val т.к. слова благодарности не меняются
 }
