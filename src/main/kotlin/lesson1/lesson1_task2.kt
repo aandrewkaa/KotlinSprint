f1f40d9ab0b331868120183d1d4a516ae5f17a58
Aandrewkaa <sa9035837863@gmail.com>
2023-06-26 19:34:04 -0300

    KS-1-2 change name style to camelCase

--- src/main/kotlin/lesson1/lesson1_task2.kt
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -3,15 +3,15 @@ package lesson1
 /** Created by Aandrewkaa **/
 
 fun main() {
-    var number_of_orders: UShort = 75U //var т.к. количество заказов меняется
-    var numbers_of_stuff: UShort = 2000U
+    var numberOfOrders: UShort = 75U //var т.к. количество заказов меняется
+    var numbersOfStuff: UShort = 2000U
 
     val gratitude_for_purchase: String = "Thank you for purchase!" //val т.к. слова благодарности не меняются
 
-    println(number_of_orders)
+    println(numberOfOrders)
     println(gratitude_for_purchase)
 //    println(numbers_of_stuff)
-    numbers_of_stuff = 1999U
-    println(numbers_of_stuff)
+    numbersOfStuff = 1999U
+    println(numbersOfStuff)
 }
 
