9bc504923e930a4b7936d3898e4026493508d136
Aandrewkaa <sa9035837863@gmail.com>
2023-06-30 12:38:14 -0300

    KS-3-3 use LocalTime for greetings

--- src/main/kotlin/lesson3/lesson3_task1.kt
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -1,15 +1,23 @@
 package lesson3
 
+import java.time.Clock
+import java.time.LocalTime
+import java.time.ZoneId
+
 /** Created by Aandrewkaa **/
 
 fun main() {
     val userName = "Андрей"
     val greetingsMorning = "Доброе утро"
     val greetingsEvening = "Добрый вечер"
+    var greetingsUser: String
+    val realTime = LocalTime.now(Clock.system(ZoneId.systemDefault())).hour
 
-    var greetingsUser = "$greetingsMorning, $userName!"
+    if (realTime <= 17) {
+        greetingsUser = "$greetingsMorning, $userName!"
+    } else {
+        greetingsUser = "$greetingsEvening, $userName!"
+    }
     println(greetingsUser)
 
-    greetingsUser = "$greetingsEvening, $userName!"
-    println(greetingsUser)
 }
