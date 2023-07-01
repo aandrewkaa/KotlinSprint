3901877d8b938f9bfb70ad264dafa15932c8ee71
Aandrewkaa <sa9035837863@gmail.com>
2023-07-01 15:32:52 -0300

    KS-4-3 use result variable for long string with condition

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -8,7 +8,9 @@ fun main() {
     val tentCondition = true
     val humidity = 20
     val seasonWinter = true
+    val result = (sunWeather == true) && (tentCondition == true) &&
+            (humidity == 20) && (seasonWinter == false)
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(sunWeather == true) && (tentCondition == true) && (humidity == 20) && (seasonWinter == false)}")
+    println("Благоприятные ли условия сейчас для роста бобовых? $result")
 
 }
