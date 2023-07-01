98f02b923fd52b924367beed31e3750af9f05e32
Aandrewkaa <sa9035837863@gmail.com>
2023-07-01 15:27:47 -0300

    KS-4-2 use start variables as constant

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -4,17 +4,22 @@ package lesson4
 /** Created by Aandrewkaa **/
 
 fun main() {
-    val weightMin = 35
-    val weightMax = 100
-    val volumeMax = 100
 
     var cargoWeight = 20
     var cargoVolume = 80
 
-    println("Average для груза с весом $cargoWeight кг и объемом $cargoVolume л: ${(cargoWeight <= weightMax) && (cargoWeight >= weightMin) && (cargoVolume < volumeMax)}")
+    println("Average для груза с весом $cargoWeight кг и объемом $cargoVolume л: " +
+            "${(cargoWeight <= WEIGHT_MAX) && (cargoWeight >= WEIGHT_MIN) && 
+                    (cargoVolume < VOLUMEMAX)}")
 
     cargoWeight = 50
     cargoVolume = 100
 
-    println("Average для груза с весом $cargoWeight кг и объемом $cargoVolume л: ${(cargoWeight <= weightMax) && (cargoWeight >= weightMin) && (cargoVolume < volumeMax)}")
+    println("Average для груза с весом $cargoWeight кг и объемом $cargoVolume л: " +
+            "${(cargoWeight <= WEIGHT_MAX) && (cargoWeight >= WEIGHT_MIN) && 
+                    (cargoVolume < VOLUMEMAX)}")
 }
+
+const val WEIGHT_MIN = 35
+const val WEIGHT_MAX = 100
+const val VOLUMEMAX = 100
