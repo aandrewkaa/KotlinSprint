fabf2597309ae995f0bca9b0724c108539fd7fc3
Aandrewkaa <sa9035837863@gmail.com>
2023-07-02 16:09:35 -0300

    KS-5-1 change printline to println

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,17 +1,13 @@
 package lesson5
 
-import java.util.*
-
 /** Created by Aandrewkaa **/
-
 
 fun main() {
     val numberFirst = 4
     val numberSecond = 5
-    val scanner = Scanner(System.`in`)
 
     print("Для входу в систему, введите в консоль сумму чисел $numberFirst + $numberSecond: ")
-    val numberRight = scanner.nextInt()
+    val numberRight = readln().toInt()
     if (numberRight == (numberFirst + numberSecond)) {
         println("Добро пожаловать!")
     } else {
